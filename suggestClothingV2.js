ac2d61702a729e57a863d95f0e6cad06cb804d7a
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-01-31 01:02:07 +0000

    add alternate code

---
+++ suggestClothingV2.js
@@ -0,0 +1,11 @@
+/*function suggestClothingV2(temperature, humidity){
+    let clothingSuggestion = "hat";
+    if(temperature >25){
+        clothingSuggestion = "baseball cap";
+    } else if (temperature >10) {
+        clothingSuggestion = "long hat"
+    }
+    return clothingSuggestion;
+}*/
+
+/// HE'S GOING TO POST THIS IN FULL ONLINE
